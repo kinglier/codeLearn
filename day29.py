b2c79c819d442c32071ff392404f29d220d6f638
kinglier <1014657423@qq.com>
2019-07-19 08:51:42 +0800

    螺旋矩阵II

--- day29.py
+++ day29.py
@@ -2,6 +2,17 @@
 #螺旋矩阵
 
 """
+Given a positive integer n, generate a square matrix filled with elements from 1 to n2 in spiral order.
+
+Example:
+
+Input: 3
+Output:
+[
+ [ 1, 2, 3 ],
+ [ 8, 9, 4 ],
+ [ 7, 6, 5 ]
+]
 """
 from typing import List
 class Solution:
